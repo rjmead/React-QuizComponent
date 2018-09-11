46fd3da341a5debc3e06d915f5afebe80eb5b786
Robert Mead <rmead@gongos.com>
2018-09-11 16:43:44 -0400

    module7

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -12,10 +12,11 @@ class QuizQuestion extends Component{
 
     handleClick(buttonText){
         if(buttonText === this.props.quiz_question.answer){
+            this.setState({incorrectAnswer: false})
             this.props.showNextQuestionHandler()
+        } else{
+            this.setState({incorrectAnswer: true})
         }
-
-        this.setState({incorrectAnswer: !this.state.incorrectAnswer})
     }
 
     render(){
